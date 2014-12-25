a67a64adb7604e46d1a9db2801be03ba176a9d72
Max Kostinevich <maxkostinevich@users.noreply.github.com>
2014-12-25 15:28:16 +0300

    Create EnableCron.sh

---
+++ EnableCron.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Autoupdate CentOS 7 with CRON
+#Description: This script will install Apache, MySQL and PHP on your server
+#Author: Max Kostinevich
+#URL: https://github.com/maxkostinevich/server-shell-scripts/
+
+clear
+
+echo 'Installing CRON..'
+# Install CRON
+yum -y install yum-cron
+chkconfig yum-cron on
+
+service yum-cron start
+
+#Final output
+clear
+echo 'CRON has been successfully installed and started'
+echo 'You can edit CRON config in /etc/yum/yum-cron.conf'
+echo 'Do not forget to restart CRON service after editing config' 
