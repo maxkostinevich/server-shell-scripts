af4aa14a406de41847361cf447840211d5433ec6
Max Kostinevich <maxkostinevich@users.noreply.github.com>
2014-12-28 12:27:25 +0300

    Fix: added Sudo

--- InstallLAMP.sh
+++ InstallLAMP.sh
@@ -16,8 +16,8 @@ serverIP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([
 #Install Apache
 echo -e "\e[1mInstalling Apache\e[0m"
 
-yum -y install httpd
-systemctl start httpd.service
+sudo yum -y install httpd
+sudo systemctl start httpd.service
 
 clear
 echo -e "\e[1mApache has been installed and started successfully\e[0m"
@@ -25,10 +25,10 @@ echo -e "\e[1mApache has been installed and started successfully\e[0m"
 #Install MySQL
 echo -e "\e[1mInstalling MySQL\e[0m"
 
-yum install mariadb-server mariadb
-systemctl start mariadb
-mysql_secure_installation
-systemctl enable mariadb.service
+sudo yum install mariadb-server mariadb
+sudo systemctl start mariadb
+sudo mysql_secure_installation
+sudo systemctl enable mariadb.service
 
 clear
 echo -e "\e[1mMySQL has been installed and started successfully\e[0m"
@@ -36,9 +36,9 @@ echo -e "\e[1mMySQL has been installed and started successfully\e[0m"
 #Install PHP
 echo -e "\e[1mInstalling PHP\e[0m"
 
-yum -y install php php-mysql
-yum -y install php-cli php-common php-fpm php-gd php-intl php-mbstring php-pear php-pecl-memcache php-xml
-systemctl restart httpd.service
+sudo yum -y install php php-mysql
+sudo yum -y install php-cli php-common php-fpm php-gd php-intl php-mbstring php-pear php-pecl-memcache php-xml
+sudo systemctl restart httpd.service
 
 clear
 echo -e "\e[1mPHP has been installed successfully\e[0m"
@@ -46,18 +46,18 @@ echo -e "\e[1mPHP has been installed successfully\e[0m"
 #Firewall
 echo -e "\e[1mUpdating Firewall\e[0m"
 
-systemctl enable firewalld
-systemctl start firewalld
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
 
-firewall-cmd --permanent --zone=public --add-service=http 
-firewall-cmd --permanent --zone=public --add-service=https
-firewall-cmd --reload
+sudo firewall-cmd --permanent --zone=public --add-service=http 
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
 
 clear
 echo -e "\e[1mFirewall has been updated successfully\e[0m"
 
 #Create test PHP-Info
-printf  '<?php phpinfo(); ?>' > /var/www/html/info.php
+sudo printf  '<?php phpinfo(); ?>' > /var/www/html/info.php
 echo '---------------------------------------------'
 echo 'info.php file has been created successfully'
 echo -e "You can check it in your browser: \e[1mhttp://${serverIP}/info.php\e[0m"
